82e4f9f400de651b75c778d30cf95481054c7f6d
Hyuck9 <lhg1304@gmail.com>
2020-02-04 10:26:39 +0900

    DataSource 셋팅값 처리 로직 변경

--- rnd-lib-database/src/main/java/com/nexmore/rnd/db/config/MariaDBConfiguration.java
+++ rnd-lib-database/src/main/java/com/nexmore/rnd/db/config/MariaDBConfiguration.java
@@ -1,33 +1,54 @@
 package com.nexmore.rnd.db.config;
 
 import com.nexmore.rnd.db.annotation.RndMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+@Slf4j
 @Configuration
 @MapperScan(basePackages = "com.nexmore.rnd.*.mapper.mariadb", annotationClass = RndMapper.class, sqlSessionFactoryRef = "mariadbSqlSessionFactory")
 public class MariaDBConfiguration {
-	@Bean
-	@Primary
-	@ConfigurationProperties("mariadb.datasource")
-	public DataSourceProperties mariadbDataSourceProperties() {
-		return new DataSourceProperties();
-	}
+
+	@Value("${mariadb.datasource.driverClassName:org.mariadb.jdbc.Driver}")
+	private String driverClassName;
+
+	@Value("${mariadb.datasource.url:jdbc:mariadb://58.229.105.141:3307/datainfo}")
+	private String url;
+
+	@Value("${mariadb.datasource.username:datainfo}")
+	private String username;
+
+	@Value("${mariadb.datasource.password:nex147200}")
+	private String password;
+
+	@Value("${mariadb.datasource.validation-query}")
+	private String validationQuery;
+
+	@Value("${mariadb.datasource.test-on-borrow}")
+	private boolean borrow;
+
 
 	@Bean
 	@Primary
 	public BasicDataSource mariadbDataSource() {
-		return mariadbDataSourceProperties().initializeDataSourceBuilder().type(BasicDataSource.class).build();
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		dataSource.setValidationQuery(validationQuery);
+		dataSource.setTestOnBorrow(borrow);
+		return dataSource;
 	}
 
 	@Bean
--- rnd-lib-database/src/main/java/com/nexmore/rnd/db/config/OracleConfiguration.java
+++ rnd-lib-database/src/main/java/com/nexmore/rnd/db/config/OracleConfiguration.java
@@ -1,33 +1,53 @@
 package com.nexmore.rnd.db.config;
 
 import com.nexmore.rnd.db.annotation.RndMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
-@SuppressWarnings("SpringFacetCodeInspection")
+@Slf4j
 @Configuration
 @MapperScan(basePackages = "com.nexmore.rnd.*.mapper.oracle", annotationClass = RndMapper.class, sqlSessionFactoryRef = "oracleSqlSessionFactory")
 public class OracleConfiguration {
 
-	@Bean
-	@ConfigurationProperties("oracle.datasource")
-	public DataSourceProperties oracleDataSourceProperties() {
-		return new DataSourceProperties();
-	}
+	@Value("${oracle.datasource.driverClassName:org.mariadb.jdbc.Driver}")
+	private String driverClassName;
+
+	@Value("${oracle.datasource.url:jdbc:mariadb://58.229.105.141:3307/datainfo}")
+	private String url;
+
+	@Value("${oracle.datasource.username:datainfo}")
+	private String username;
+
+	@Value("${oracle.datasource.password:nex147200}")
+	private String password;
+
+	@Value("${oracle.datasource.validation-query}")
+	private String validationQuery;
+
+	@Value("${oracle.datasource.test-on-borrow}")
+	private boolean borrow;
 
 	@Bean
-	public DataSource oracleDataSource() {
-		return oracleDataSourceProperties().initializeDataSourceBuilder().build();
+	public BasicDataSource oracleDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		dataSource.setTestOnBorrow(borrow);
+		dataSource.setValidationQuery(validationQuery);
+		return dataSource;
 	}
 
 	@Bean(name = "oracleSqlSessionFactory")
